6df52e9dde036daa75924e32d2c867f07a5b7de0
onesuchkeeper <80510210+onesuchkeeper@users.noreply.github.com>
2021-03-12 00:05:21 -0800

    Added cs

---
+++ IHp2BaseModStarter.cs
@@ -0,0 +1,13 @@
+﻿namespace Hp2BaseMod.Interface
+{
+    /// <summary>
+    /// Used to define starting point of mods. The base mod will run the start method.
+    /// </summary>
+    public interface IHp2BaseModStarter
+    {
+        /// <summary>
+        /// Starts the mod, called by the Hp2BaseMod
+        /// </summary>
+        abstract void Start();
+    }
+}
