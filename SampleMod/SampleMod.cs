54a60daba00f3523342d4c58e1113a4f81dad091
onesuchkeeper <80510210+onesuchkeeper@users.noreply.github.com>
2021-03-12 00:31:22 -0800

    Added SampleMod
    an example for mod development and testing

---
+++ SampleMod/SampleMod.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Hp2BaseMod.Interface;
+
+namespace Hp2SampleMod
+{
+    public class SampleMod : IHp2BaseModStarter
+    {
+        public void Start()
+        {
+            TextWriter tw = File.CreateText("Hp2SampleModOutput.txt");
+            tw.WriteLine("Hello world!");
+            tw.Flush();
+        }
+    }
+}
